f80289e2bfbac7b968f6b5f5b857fc4496518e45
o-seoj <tjwjd010@naver.com>
2025-10-31 17:49:14 +0900

    add 10/31

--- ch09/src/main/java/kr/co/ch09/controller/UserController.java
+++ ch09/src/main/java/kr/co/ch09/controller/UserController.java
@@ -33,6 +33,7 @@ public class UserController {
 
     @PostMapping("/user/login")
     public ResponseEntity<Map<String, String>> login(@RequestBody UserDTO userDTO) {
+        log.info("Login User: {}", userDTO);
 
         // 시큐리티 인증처리
         UsernamePasswordAuthenticationToken authToken
